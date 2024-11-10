18e19eb4119af255bbd95fbed0a0e9a9fe4e8b6f
qeafs <mohamadhguhf@gmail.com>
2024-11-11 02:42:18 +0300

    2

---
+++ ProcessTable.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class ProcessTable {
+    
+    public ArrayList<ProcessTable> pcb = new ArrayList<ProcessTable>();
+    int id;
+    int time;
+    int memory;
+
+}
