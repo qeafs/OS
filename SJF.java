7d2693d3c827f8864923291666a91e3ea973af35
qeafs <mohamadhguhf@gmail.com>
2024-11-11 00:55:46 +0300

    Update SJF.java

--- SJF.java
+++ SJF.java
@@ -1,3 +1,10 @@
-public class SJF {
+public class SJF extends Thread{
+    int freememory = 1024;
     
+
+
+    @Override
+    public void run(){
+        // here the thred code will start
+    }
 }
