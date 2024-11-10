426d62342334143941a49fb1db44aa41f6d92a27
qeafs <mohamadhguhf@gmail.com>
2024-11-11 02:59:43 +0300

    Update Filereader.java

--- Filereader.java
+++ Filereader.java
@@ -3,12 +3,10 @@ import java.util.ArrayList;
 public class Filereader extends Thread{
     
     public ArrayList<ProcessTable> pcb = new ArrayList<ProcessTable>();
-
 //array list each item is an pbject from Process table each object contains the neccecarry infrormation 
 // the whole array list IS the process Table 
-        ProcessTable pcb1 = new ProcessTable(1,30,4);
 
-        
+        ProcessTable pcb1 = new ProcessTable(1,30,4);
 
 
 
