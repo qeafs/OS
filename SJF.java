6555141aa2ecf6b9a67b0e29876b5a2019cf0920
qeafs <mohamadhguhf@gmail.com>
2024-11-11 13:07:02 +0300

    Update SJF.java

--- SJF.java
+++ SJF.java
@@ -9,7 +9,8 @@ public class SJF implements Runnable{
     public void run(){
         ProcessTable sj = new ProcessTable(Filereader.getMyList().get(0)); //shortest job is first job right now
         for(int i=0; i<Filereader.getMyList().size(); i++){
-           
+           if(sj.getTime() > Filereader.getMyList().get(i).time)
+           sj = Filereader.getMyList().get(i);
         }
        
 
