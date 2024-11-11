315bc292dcd21130d7774e53ba01646b7caffc00
qeafs <mohamadhguhf@gmail.com>
2024-11-11 13:22:48 +0300

    Update SJF.java

--- SJF.java
+++ SJF.java
@@ -3,14 +3,23 @@ import java.util.ArrayList;
 public class SJF implements Runnable{
     
     int freememory = 1024;
+    public static ArrayList<ProcessTable> readyqueue = new ArrayList<ProcessTable>();
 
 
     @Override
     public void run(){
-        ProcessTable sj = new ProcessTable(Filereader.getMyList().get(0)); //shortest job is first job right now
+        
+        ProcessTable sj = new ProcessTable(Filereader.getMyList().get(0)); 
+        //shortest job is first job right now
+
+        //find the shortest job in the jobqueue.
         for(int i=0; i<Filereader.getMyList().size(); i++){
            if(sj.getTime() > Filereader.getMyList().get(i).time)
            sj = Filereader.getMyList().get(i);
+        }
+        if(freememory >= sj.memory){
+            freememory = freememory - sj.memory;
+            readyqueue.add(sj);
         }
        
 
