93b044a4c75f814cf37392a75ba2691d128c5bdd
qeafs <mohamadhguhf@gmail.com>
2024-11-12 21:10:02 +0300

    Update SJF.java

--- SJF.java
+++ SJF.java
@@ -3,14 +3,15 @@ import java.util.ArrayList;
 public class SJF implements Runnable{
     
     public static int freememory = 1024;
+    public int avreagewaitingtime =0;
     public static ArrayList<PCB> readyqueue = new ArrayList<PCB>();
 
     public static ArrayList<PCB> getMyList() {
         return readyqueue;
-  }   
-    public static int getFreememory(){
+        }   
+    public static int getfreememory(){
         return freememory;
-    }
+            }       
 
     @Override
     public void run(){
@@ -19,7 +20,7 @@ public class SJF implements Runnable{
         //shortest job is first job right now
 
         //find the shortest job in the jobqueue.
-        while(true){
+        while(!Filereader.getMyList().isEmpty()){
         for(int i=0; i<Filereader.getMyList().size(); i++){
            if(sj.bursttime > Filereader.getMyList().get(i).bursttime)
            sj = Filereader.getMyList().get(i);
@@ -27,10 +28,8 @@ public class SJF implements Runnable{
         while(freememory < sj.memory);
             freememory = freememory - sj.memory;
            
-            readyqueue.add(sj);
-            Filereader.getMyList().remove(sj);
-        
-       
+            readyqueue.add(sj);//add to ready queue.
+            Filereader.getMyList().remove(sj); //remove from job queue.
 
     }
 }
