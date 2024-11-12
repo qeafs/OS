d8039f0d0d13dcb073cc6185b86960ee53eec309
Loha25 <little1man@outlook.com>
2024-11-12 23:52:23 +0300

    Abdulelah 2
    hello

--- FCFS.java
+++ FCFS.java
@@ -1,11 +1,53 @@
+import java.util.LinkedList;
+import java.util.Queue;
 public class FCFS implements Runnable{
     
     int freememory = 1024;
+    private Queue<PCB> readyQueue;
+    
 
 
+public FCFS(){
+    readyQueue = new LinkedList<>();
+}
+public void addProcess(PCB process){
+    if(process.getMemory() <= freememory){
+        readyQueue.offer(process);
+        freememory -= process.getMemory();
+        System.out.println("Process " + process.getId() + " added to ready queue");
+    }
+    else{
+        System.out.println("Not enough memory");
+    }
+}
 
     @Override
     public void run(){
+        while(true){
+            if(!readyQueue.isEmpty()){
+                PCB currentProcess = readyQueue.poll();
+                System.out.println("Executing process " + currentProcess.getId());
+
+                try {
+                    Thread.sleep(currentProcess.getBursttime() * 1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    System.out.println("Process execution interrupted: " + e.getMessage());
+                }
+
+                freememory += currentProcess.getMemory();
+                System.out.println("Process " + currentProcess.getId() + " finished");
+            }
+            try{
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                System.out.println("Thread interrupted: " + e.getMessage());
+            }
+
+
+        }
 
     }
+
 }
